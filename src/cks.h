e07289d125c94349a98eb5d4eab46065773fa538
realkorosh2000 <maki.korosh@gmail.com>
2025-11-14 09:45:30 +0330

    new version step 3

--- src/cks.h
+++
@@ -1,9 +0,0 @@
-#ifndef CKS_H
-#define CKS_H
-
-void setFont(const char *font);
-void setColor(const char *color);
-void importCK(const char *filename);
-void executeCKSFile(const char *filename);
-
-#endif
